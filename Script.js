866173b47235a8482c7bf774d961853d55057c60
charanmutyala <charanmutyala4@gmail.com>
2023-07-19 12:08:39 +0530

    hi

---
+++ Script.js
@@ -0,0 +1,10 @@
+function bulboff()
+{
+document.getElementById("Bulb").src="https://www.livehome3d.com/assets/img/articles/comparinglightbulbtypes/incandescent-bulb@2x.jpg"
+
+}
+function bulbonn()
+{
+document.getElementById("Bulb").src="https://previews.123rf.com/images/murika/murika1511/murika151100069/48123160-bright-glowing-incandescent-light-bulb-on-a-white-background.jpg"
+
+}
